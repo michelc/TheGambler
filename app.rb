4ea1703f65c4139eb3847be9b61fafe13986c947
michelc <michelc@gmail.com>
2013-02-19 19:47:52 +0100

    Première version

---
+++ app.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+
+require "sinatra"
+require "open-uri"
+
+require "sinatra/reloader" if development?
+
+get "/chifoumi" do
+
+  # Récupération des données
+  partie = params[:Game]      # Nom de la partie
+  tour = params[:MoveId]      # ID du tour en cours
+  arbitre = params[:Referee]  # URL pour répondre à l'arbitre
+
+  # Calcul du coup à jouer
+  coup = rand(1..3)
+
+  # Envoie le coup à l'arbitre
+  url = "#{arbitre}?Game=#{partie}&MoveId=#{tour}&Value=#{coup}"
+  # open(url).read.size
+end
---
+++ readme.md
@@ -0,0 +1,8 @@
+The Gambler
+-----------
+
+Moteur de jeu développé en Ruby + Sinatra pour participer au concours
+[tournoyons.developpez.com](http://tournoyons.developpez.com).
+
+Pour l'instant, seul le jeu du Chifoumi est implémenté pour tester que
+l'interface avec le serveur fonctionne.
